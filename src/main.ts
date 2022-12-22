b4995ce8816893b6735069d92de0cf4d51df1b64
ondrui <ondrui@yandex.ru>
2022-12-22 13:33:12 +0300

    add set devServer to vue.config.js

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
+import router from "./router";
 import BootstrapVue3 from "bootstrap-vue-3";
 
 // Optional, since every component imports their Bootstrap functionality
@@ -9,7 +10,6 @@ import BootstrapVue3 from "bootstrap-vue-3";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
-import router from "./router";
 
 const app = createApp(App).use(router);
 app.use(BootstrapVue3);
